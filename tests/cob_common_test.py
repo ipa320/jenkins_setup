e30517c8cfb6412ec261d3302d78ad2c8d3fabeb
fmw-jk <jannik.kett@googlemail.com>
2013-01-09 19:08:47 +0100

    test framework

---
+++ tests/cob_common_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import unittest
+from jenkins_setup import cob_common
+
+
+class Cobdistro_Test(unittest.TestCase):
+
+    def setUp(self):
+        self.MaxDiff = None
+
+
+if __name__ == "__main__":
+    unittest.main()
