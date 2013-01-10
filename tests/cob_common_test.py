025fd82e7685ef828c878a58a7bab240e249a5ee
fmw-jk <jannik.kett@googlemail.com>
2013-01-10 11:36:30 +0100

    tests for 'cob_common.get_buildpipeline_configs'

--- tests/cob_common_test.py
+++ tests/cob_common_test.py
@@ -4,11 +4,21 @@ import unittest
 from jenkins_setup import cob_common
 
 
-class Cobdistro_Test(unittest.TestCase):
+class Cob_Common_Test(unittest.TestCase):
 
     def setUp(self):
         self.MaxDiff = None
 
+    def test__get_buildpipeline_configs__input_pipeline_name_string__return_configs_dict(self):
+        result = cob_common.get_buildpipeline_configs('test_user__jenkins_test_server')
+        self.assertEqual(type(result), dict)
+
+    def test__get_buildpipeline_configs__input_pipeline_name_string__check_entries(self):
+        result = cob_common.get_buildpipeline_configs('test_user__jenkins_test_server')
+        self.assertEqual(result['user_name'], 'test_user')
+
+    def test__get_buildpipeline_configs__input_wrong_pipeline_name__raise_exception(self):
+        self.assertRaises(Exception, cob_common.get_buildpipeline_configs, 'wrong_pipeline_name')
 
 if __name__ == "__main__":
     unittest.main()
