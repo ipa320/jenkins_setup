5917d994794feed2e92693f9a63edead0ba2b639
fmw-jk <jannik.kett@googlemail.com>
2013-01-11 09:21:24 +0100

    followed python style guide and empty test for rosdep

---
+++ tests/rosdep_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import unittest
+import rosdep
+
+
+class Rosdep_Test(unittest.TestCase):
+
+    def setUp(self):
+        self.MaxDiff = None
+
+
+if __name__ == "__main__":
+    unittest.main()
