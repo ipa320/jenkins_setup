98ac2d4b8c0e4981763c408562db2314934d124f
fmw-jk <jannik.kett@googlemail.com>
2013-01-16 14:51:33 +0100

    adapted tests to new method input

--- tests/cob_common_test.py
+++ tests/cob_common_test.py
@@ -9,16 +9,23 @@ class Cob_Common_Test(unittest.TestCase):
     def setUp(self):
         self.MaxDiff = None
 
-    def test__get_buildpipeline_configs__input_pipeline_name_string__return_configs_dict(self):
-        result = cob_common.get_buildpipeline_configs('test_user__jenkins_test_server')
+    def test__get_dependencies_for_dry_repos__input_source_folder_str__return_dependencies_list(self):
+        pass
+
+    def test__get_buildpipeline_configs__input_server_and_user_name_string__return_configs_dict(self):
+        result = cob_common.get_buildpipeline_configs('jenkins_test_server', 'test_user')
         self.assertEqual(type(result), dict)
 
-    def test__get_buildpipeline_configs__input_pipeline_name_string__check_entries(self):
-        result = cob_common.get_buildpipeline_configs('test_user__jenkins_test_server')
+    def test__get_buildpipeline_configs__input_server_and_user_name_string__check_entries(self):
+        result = cob_common.get_buildpipeline_configs('jenkins_test_server', 'test_user')
         self.assertEqual(result['user_name'], 'test_user')
 
-    def test__get_buildpipeline_configs__input_wrong_pipeline_name__raise_exception(self):
-        self.assertRaises(Exception, cob_common.get_buildpipeline_configs, 'wrong_pipeline_name')
+    def test__get_buildpipeline_configs__input_wrong_name__raise_exception(self):
+        self.assertRaises(Exception, cob_common.get_buildpipeline_configs, 'wrong_name', 'test_user')
+
+    def test__get_buildpipeline_configs__input_wrong_name__raise_exception2(self):
+        self.assertRaises(Exception, cob_common.get_buildpipeline_configs, 'jenkins_test_server', 'wrong_name')
+
 
 if __name__ == "__main__":
     unittest.main()
