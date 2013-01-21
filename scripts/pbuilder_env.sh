5aa67ad7d2f04cebe9183f19ba774812d7db9633
fmw-jk <jannik.kett@googlemail.com>
2013-01-21 13:50:01 +0100

    Output

--- scripts/pbuilder_env.sh
+++ scripts/pbuilder_env.sh
@@ -1,7 +1,9 @@
-#!/bin/bash -ex
+#!/bin/bash -e
+echo "vvvvvvvvvvvvvvvvvvv  pbuilder_env.sh vvvvvvvvvvvvvvvvvvvvvv"
 export WORKSPACE=$1
 echo $WORKSPACE
 
+echo "Set up environment variables"
 . $WORKSPACE/env_vars.sh
 
 export PATH=$PATH:/usr/local/bin
@@ -9,14 +11,26 @@ export PATH=$PATH:/usr/local/bin
 export ROS_PACKAGE_PATH=/tmp/test_repositories/src_repository:$ROS_PACKAGE_PATH
 export PYTHONPATH=$WORKSPACE/jenkins_setup/src:$PYTHONPATH
 
+env
+
+echo "Set up git and ssh"
 cp $WORKSPACE/.gitconfig ~/.gitconfig
 cp -a $WORKSPACE/.ssh /root
 chown -R root.root /root/.ssh
 
+echo "Install python-catkin-pkg python-rosdistro rosinstall"
 apt-get update
 apt-get upgrade -y
 apt-get install python-catkin-pkg python-rosdistro -y
 
 pip install -U rosinstall
 
-$WORKSPACE/jenkins_setup/scripts/build.py $JENKINS_MASTER $JENKINS_USER $ROSDISTRO $REPOSITORY
+echo "============================================================"
+echo "==== Begin" $SCRIPT "script.    Ignore the output above ===="
+echo "============================================================"
+
+$WORKSPACE/jenkins_setup/scripts/${JOBTYPE}.py $JENKINS_MASTER $JENKINS_USER $ROSDISTRO $REPOSITORY
+
+echo "============================================================"
+echo "==== End" $SCRIPT "script.    Ignore the output below ======"
+echo "============================================================"
