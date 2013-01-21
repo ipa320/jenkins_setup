c331c26633cb42dd439968a36acaaa18999e85a8
fmw-jk <jannik.kett@googlemail.com>
2013-01-21 12:03:14 +0100

    ROS_PACKAGE_PATH, PYTHONPATH, .gitconfig, .ssh

--- scripts/pbuilder_env.sh
+++ scripts/pbuilder_env.sh
@@ -1,23 +1,30 @@
 #!/bin/bash -ex
-WORKSPACE=$1
+export WORKSPACE=$1
 echo $WORKSPACE
 
 env
 
-cd
-ls -lah
-cd $WORKSPACE 
-ls -lah
-
-. ./env_vars.sh
+. $WORKSPACE/env_vars.sh
 
 export PATH=$PATH:/usr/local/bin
 . /opt/ros/$ROSDISTRO/setup.sh
+export ROS_PACKAGE_PATH=/tmp/test_repositories/src_repository
+export PYTHONPATH=$WORKSPACE/jenkins_setup/src:$PYTHONPATH
 
 env
+
+cp $WORKSPACE/.gitconfig ~/.gitconfig
+cp -a $WORKSPACE/.ssh /root
+chown -R root.root /root/.ssh
 
 ls -lah
 
 echo "Test"
+
+cd
+
+ls -lah
+
+cd $WORKSPACE
 
 ls -lah
