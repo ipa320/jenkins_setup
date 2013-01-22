ee66888cc2751be77c2c5026bd346befba1ea3af
fmw-jk <jannik.kett@googlemail.com>
2013-01-22 13:29:22 +0100

    new job creator plus corresponding tests

---
+++ src/jenkins_setup/jenkins_job_creator.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python
+
+import datetime
+import socket
+import pkg_resources
+import yaml
+import re
+
+
+class Jenkins_Job(object):
+    """
+    Object representation of Jenkins job
+    """
+    def __init__(self, jenkins_instance, pipeline_config):
+        """
+        Sets up Jenkins job object
+        """
+
+        self.JOB_TYPE_NAMES = {'pipe': 'pipe_starter',
+                               'prio': 'prio_build',
+                               'normal': 'normal_build',
+                               'down': 'downstream_build',
+                               'db': 'database_test',
+                               'sim': 'simulation_test',
+                               'app': 'application_test',
+                               'clean': 'cleanup',
+                               'bringup': 'bringup_hardware_test',
+                               'hilevel': 'highlevel_hardware_test',
+                               'release': 'release_params'}
+
+        self.jenkins_instance = jenkins_instance
+        self.pipe_conf = pipeline_config
+
+        self.job_config_params = pkg_resources.resource_string('jenkins_setup', 'templates/job_config_params.yaml')
+        self.job_config_params = yaml.load(self.job_config_params)
+        self.job_config = pkg_resources.resource_string('jenkins_setup', 'templates/job_config.xml')
+
+    def create_job(self):
+        """
+        Configurates the job by replacing the templates placeholder
+        """
+
+        self.get_common_params()
+
+    def schedule_job(self):
+        """
+        Create new or reconfigure existent job
+        """
+        if self.jenkins_instance.job_exists(self.job_name):
+            try:
+                self.jenkins_instance.reconfig_job(self.job_name, self.job_config)
+                print "Reconfigured job %s" % self.job_name
+                return 'reconfigured'
+            except:
+                return 'reconfiguration failed'
+        else:
+            try:
+                self.jenkins_instance.create_job(self.job_name, self.job_config)
+                print "Created job %s" % self.job_name
+                return 'created'
+            except:
+                return 'creation failed'
+
+    def delete_job(self, job_name):
+        """
+        Deletes the job defined by the job name
+
+        :param job_name: name of the job to delete, ``str``
+        :returns: return message, ``str``
+        """
+
+        if self.jenkins_instance.job_exists(job_name):
+            try:
+                self.jenkins_instance.delete_job(job_name)
+            except:
+                return 'deletion failed'  # Exception ??
+            return 'deleted'
+        else:
+            return 'not existent'
+
+    def get_common_params(self):
+        """
+        Gets all parameters which have to be defined for every job type
+        """
+
+        self.params = {}
+
+        self.params['USERNAME'] = self.pipe_conf['user_name']
+        self.params['EMAIL'] = self.pipe_conf['email']
+        self.params['EMAIL_COMMITTER'] = self.pipe_conf['committer']
+        self.params['JOB_TYPE_NAME'] = self.JOB_TYPE_NAMES[self.job_type]
+        self.params['SCRIPT'] = self.JOB_TYPE_NAMES[self.job_type]
+        self.params['NODE_LABEL'] = self.job_type
+        self.params['TIME'] = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M')
+        self.params['HOSTNAME'] = socket.gethostname()
+        self.params['ROSDISTRO'] = self.pipe_conf['ros_distro']
+        self.params['PROJECT'] = 'matrix-project'
+        self.params['TRIGGER'] = self.job_config_params['triggers']['none']
+        self.params['VCS'] = self.job_config_params['vcs']['none']
+        self.params['MATRIX'] = ''
+        self.params['PARAMETERS'] = ''
+        self.params['POSTBUILD_TRIGGER'] = self.params['JOIN_TRIGGER'] = self.params['PIPELINE_TRIGGER'] = self.params['GROOVY_POSTBUILD'] = ''
+
+    ###########################################################################
+    # helper methods - parameter generation
+    ###########################################################################
+    def replace_placeholder(self, job_config, params):
+        '''replace placeholder in template with params'''
+        for key, value in params.iteritems():
+            if "@(%s)" % key not in job_config:
+                raise KeyError("Parameter %s could not be replaced, because it is not existent" % key)
+            job_config = job_config.replace("@(%s)" % key, value)
+        not_replaced_keys = re.findall('@\(([A-Z0-9_]+)\)', job_config)
+        if not_replaced_keys != []:
+            raise KeyError("The keys %s were not replaced, because the parameters where missing" % (str(not_replaced_keys)))
+        return job_config
+
+    def generate_job_name(self, job_type, suffix=''):
+        '''
+        Returns a job_name string generated from a job_type plus suffix
+
+        :param job_type: short job type name, ``str``
+        :param suffix: will be added to the end of the name, ``str``
+        '''
+
+        if suffix != '':
+            return '__'.join([self.pipe_conf['user_name'],
+                              self.JOB_TYPE_NAMES[job_type],
+                              suffix])
+        else:
+            return '__'.join([self.pipe_conf['user_name'],
+                              self.JOB_TYPE_NAMES[job_type]])
+
+    def generate_job_list(self, job_type_list):
+        '''returns a list of job_names generated from a list of job_types'''
+        if type(job_type_list) != list:
+            raise TypeError("Input type is not type 'list'")
+        return [self.generate_job_name(job_type) for job_type in job_type_list]
+
+    def generate_job_list_string(self, job_type_list):
+        '''returns a string of comma separated job_names generated from a list of job_types'''
+        return ', '.join(self.generate_job_list(job_type_list))
+
+    def generate_distro_arch_field(self):
+        '''TODO'''
+        result = {}
+        result['ros_distro'] = self.pipe_conf['ros_distro']
+        result['ubuntu_distro'] = []
+        for ros_distro in result['ros_distro']:
+            for ubuntu_distro in self.TARGET_PLATFORM[ros_distro]:
+                if ubuntu_distro not in result['ubuntu_distro']:
+                    result['ubuntu_distro'].append(ubuntu_distro)
+        result['arch'] = self.TARGET_ARCH
+        return result
+
+    def generate_prio_distro_arch(self):
+        '''TODO'''
+        result = []
+        for ros_distro in self.pipe_conf['ros_distro']:
+            entry = {}
+            entry['ros_distro'] = ros_distro
+            entry['ubuntu_distro'] = self.PRIO_DISTRO[ros_distro]
+            entry['arch'] = self.PRIO_ARCH
+            if ros_distro in self.pipe_conf['prio_distro_arch']:
+                entry['ubuntu_distro'] = self.pipe_conf['prio_distro_arch'][ros_distro]['ubuntu_distro']
+                entry['arch'] = self.pipe_conf['prio_distro_arch'][ros_distro]['arch']
+
+            result.append(entry)
+
+        return result
+
+    def generate_matrix_filter(self, config, negation=False):
+        '''TODO'''
+        if negation:
+            filter = '!(%s)' % ' || '.join(['(%s)' % ' && '.join(['%s == %s' % (key, value)
+                                                                  for key, value in i.iteritems()])
+                                            for i in config])
+        else:
+            filter = '(%s)' % ' || '.join(['(%s)' % ' && '.join(['%s == %s' % (key, value)
+                                                                 for key, value in i.iteritems()])
+                                           for i in config])
+
+        return filter
+
+    def generate_matrix_axis(self, axis_name, value_list):
+        ''' TODO '''
+
+        if axis_name == '':
+            raise Exception('No proper name given')
+        if value_list == []:
+            raise Exception('No values given')
+        axis = self.job_config_params['matrix']['axis'].replace('@(NAME)', axis_name)
+        values = ' '.join([self.job_config_params['matrix']['value'].replace('@(VALUE)', value) for value in value_list])
+        axis = axis.replace('@(VALUES)', values)
+
+        return axis
+
+    def generate_matrix_param(self, name_value_dict, filter=None):
+        ''' TODO '''
+
+        axes = ''
+        if name_value_dict == {}:
+            return ''
+        axes += ' '.join([self.generate_matrix_axis(axis_name, axis_values)
+                          for axis_name, axis_values in name_value_dict.iteritems()])
+
+        matrix = self.job_config_params['matrix']['basic'].replace('@(AXES)', axes)
+        if filter:
+            matrix += ' ' + self.job_config_params['matrix']['filter'].replace('@(FILTER)', filter)
+
+        return matrix
+
+    def generate_jointrigger_param(self, job_type_list, unstable_behavior):
+        ''' TODO '''
+        if job_type_list == []:
+            return ''
+        elif unstable_behavior == '':
+            raise Exception('No behavior for unstable job result')
+        elif not (unstable_behavior == 'true' or unstable_behavior == 'false'):
+            raise Exception("Behavior argument has to be 'true' or 'false'")
+
+        jointrigger = self.job_config_params['jointrigger'].replace('@(JOIN_PROJECTS)',
+                                                                    self.generate_job_list_string(job_type_list))
+        return jointrigger.replace('@(JOIN_UNSTABLE)', str(unstable_behavior))
+
+    def generate_postbuildtrigger_param(self, job_type_list, threshold_name):
+        ''' TODO '''
+        if job_type_list == []:
+            return ''
+        elif threshold_name == '':
+            raise Exception('No treshold for postbuildtrigger given')
+        elif threshold_name not in ['SUCCESS', 'UNSTABLE', 'FAILURE']:  # TODO check tresholds
+            raise Exception("Threshold argument invalid")
+
+        postbuildtrigger = self.job_config_params['postbuildtrigger'].replace('@(CHILD_PROJECTS)',
+                                                                              self.generate_job_list_string(job_type_list))
+        return postbuildtrigger.replace('@(THRESHOLD)', threshold_name)
+
+    def generate_pipelinetrigger_param(self, job_type_list):
+        ''' TODO '''
+        if job_type_list == []:
+            return ''
+        return self.job_config_params['pipelinetrigger'].replace('@(PIPELINETRIGGER_PROJECT)',
+                                                                 self.generate_job_list_string(job_type_list))
+
+    def generate_groovypostbuild_param(self, script_type, project_list, behavior):
+        ''' TODO '''
+        if project_list == []:
+            raise Exception('No project is given')
+        if behavior > 2 or behavior < 0 or type(behavior) != int:
+            raise Exception('Invalid behavior number given')
+        script = self.job_config_params['groovypostbuild']['script'][script_type].replace('@(PROJECT_LIST)',
+                                                                                          str(self.generate_job_list(project_list)))
+        return self.job_config_params['groovypostbuild']['basic'].replace('@(GROOVYPB_SCRIPT)', script).replace('@(GROOVYPB_BEHAVIOR)', str(behavior))
+
+
+class Pipe_Starter_Job(Jenkins_Job):
+    """
+    Object representation of Pipe Starter Job
+    """
+    def __init__(self, jenkins_instance):
+        """
+        """
+
+        super(Jenkins_Job, self).__init__(jenkins_instance)
---
+++ tests/jenkins_job_creator_test.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+
+import unittest
+
+import os
+import yaml
+import jenkins
+
+from jenkins_setup import jenkins_job_creator, cob_common
+
+
+class Jenkins_Job_Test(unittest.TestCase):
+    """
+    Tests Jenkins jobs
+    """
+
+    def setUp(self):
+        self.maxDiff = None
+
+        self.test_dict = cob_common.get_buildpipeline_configs('jenkins-test-server', 'test-user')
+
+        self.job_type_test_list = ['pipe', 'prio', 'normal']
+
+        with open(os.path.expanduser('~/jenkins-config/slave_config.yaml')) as f:
+            info = yaml.load(f)
+        jenkins_instance = jenkins.Jenkins(info['master_url'], info['jenkins_login'], info['jenkins_pw'])
+
+        self.jj = jenkins_job_creator.Jenkins_Job(jenkins_instance, self.test_dict)
+
+    # Testing replace_placeholder
+    def test__replace_placeholder__input_config_string_and_params_dict__resturn_config_string(self):
+        to_replace = '@(TEST_1) and @(TEST_2) should be replaced'
+        result = self.jj.replace_placeholder(to_replace, {'TEST_1': 'THIS', 'TEST_2': 'THAT'})
+        self.assertEqual(result, 'THIS and THAT should be replaced')
+
+    def test__replace_placeholder__input_not_existent_key__raise_exception(self):
+        to_replace = '@(TEST_1) and @(TEST_2) should be replaced'
+        self.assertRaises(KeyError, self.jj.replace_placeholder, to_replace, {'WRONG': 'THIS', 'TEST_2': 'THAT'})
+
+    def test__replace_placeholder__missing_parameter__raise_exception(self):
+        to_replace = '@(TEST_1) and @(TEST_2) should be replaced'
+        self.assertRaises(KeyError, self.jj.replace_placeholder, to_replace, {'TEST_1': 'THIS'})
+
+    def test__replace_placeholder__missing_parameter__raise_exception2(self):
+        to_replace = '@(TEST_1) and @(TEST_2) should be @( replaced'
+        result = self.jj.replace_placeholder(to_replace, {'TEST_1': 'THIS', 'TEST_2': 'THAT'})
+        self.assertEqual(result, 'THIS and THAT should be @( replaced')
+
+    # Testing generate_job_name
+    def test__generate_job_name__input_job_string__return_job_name_string(self):
+        result = self.jj.generate_job_name('pipe')
+        self.assertEqual(result, 'test-user__pipe_starter')
+
+    def test__generate_job_name__input_job_string__return_job_name_string2(self):
+        result = self.jj.generate_job_name('hilevel')
+        self.assertEqual(result, 'test-user__highlevel_hardware_test')
+
+    def test__generate_job_name__input_job_and_suffix_string__return_job_name_string(self):
+        result = self.jj.generate_job_name('pipe', 'suffix')
+        self.assertEqual(result, 'test-user__pipe_starter__suffix')
+
+    def test__generate_job_name__input_wrong_key_string__raise_exception(self):
+        self.assertRaises(KeyError, self.jj.generate_job_name, 'wrong_key')
+
+    def test__generate_job_name__input_wrong_key_int__raise_exception(self):
+        self.assertRaises(KeyError, self.jj.generate_job_name, 12)
+
+    # Testing generate_job_list
+    def test__generate_job_list__input_job_list__return_job_name_list(self):
+        result = self.jj.generate_job_list(self.job_type_test_list)
+        self.assertEqual(result, ['test-user__pipe_starter',
+                                  'test-user__prio_build',
+                                  'test-user__normal_build'])
+
+    def test__generate_job_list__input_empty_list__return_empty_string(self):
+        result = self.jj.generate_job_list([])
+        self.assertEqual(result, [])
+
+    def test__generate_job_list__input_dict__raise_exception(self):
+        self.assertRaises(TypeError, self.jj.generate_job_list, ('pipe', 'prio', 'normal'))
+
+    # Testing generate_job_list_string
+    def test__generate_job_list_string__input_job_list__return_job_names_as_comma_separatet_string(self):
+        result = self.jj.generate_job_list_string(self.job_type_test_list)
+        self.assertEqual(result, 'test-user__pipe_starter, test-user__prio_build, test-user__normal_build')
+
+    def test__generate_job_list_string__input_empty_list__return_empty_string(self):
+        result = self.jj.generate_job_list_string([])
+        self.assertEqual(result, '')
+
+    # Testing generate_matrix_axis
+    def test__generate_matrix_axis__input_name_string_and_value_list__return_axis_config_string(self):
+        axis_name = 'test_axis'
+        value_list = ['test_value_1', 'test_value_2', 'test_value_3']
+        result = self.jj.generate_matrix_axis(axis_name, value_list)
+        self.assertEqual(result, '<hudson.matrix.TextAxis> <name>test_axis</name> <values> <string>test_value_1</string> <string>test_value_2</string> <string>test_value_3</string> </values> </hudson.matrix.TextAxis>')
+
+    def test__generate_matrix_axis__input_empty_list__raise_exception(self):
+        axis_name = 'test_axis'
+        self.assertRaises(Exception, self.jj.generate_matrix_axis, axis_name, [])
+
+    def test__generate_matrix_axis__input_empty_name__raise_exception(self):
+        value_list = ['test_value_1', 'test_value_2', 'test_value_3']
+        self.assertRaises(Exception, self.jj.generate_matrix_axis, '', value_list)
+
+    # Testing generate_matrix_param
+    def test__generate_matrix_param__input_name_value_dict_and_filter_string__return_matrix_config_string(self):
+        name_value_test_dict = {'test_axis': ['test_value_1', 'test_value_2', 'test_value_3']}
+        result = self.jj.generate_matrix_param(name_value_test_dict, 'filter')
+        self.assertEqual(result, '<axes> <hudson.matrix.TextAxis> <name>test_axis</name> <values> <string>test_value_1</string> <string>test_value_2</string> <string>test_value_3</string> </values> </hudson.matrix.TextAxis> </axes> <combinationFilter>filter</combinationFilter>')
+
+    def test__generate_matrix_param__input_empty_name_value_dict__raise_exception(self):
+        self.assertEqual(self.jj.generate_matrix_param({}, 'filter'), '')
+
+    def test__generate_matrix_param__input_empty_value_list__raise_exception(self):
+        name_value_test_dict = {'test_axis': []}
+        self.assertRaises(Exception, self.jj.generate_matrix_param, name_value_test_dict, 'filter')
+
+    def test__generate_matrix_param__input_list_istead_of_name_value_dict__raise_exception(self):
+        self.assertRaises(AttributeError, self.jj.generate_matrix_param, ['test_value_1', 'test_value_2', 'test_value_3'], 'filter')
+
+    def test__generate_matrix_param__input_empty_filter_string__return_matrix_config_string_without_filter(self):
+        name_value_test_dict = {'test_axis': ['test_value_1', 'test_value_2', 'test_value_3']}
+        self.assertTrue('combinationFilter' not in self.jj.generate_matrix_param(name_value_test_dict))
+
+    # Testing generate_jointtrigger_param
+    def test__generate_jointrigger_param__input_job_type_list_and_unstable_behavior_string__return_jointrigger_config_string(self):
+        unstable_behavior_test = 'true'
+        result = self.jj.generate_jointrigger_param(self.job_type_test_list, unstable_behavior_test)
+        self.assertEqual(result, '<join.JoinTrigger> <joinProjects>test-user__pipe_starter, test-user__prio_build, test-user__normal_build</joinProjects> <joinPublishers/> <evenIfDownstreamUnstable>true</evenIfDownstreamUnstable> </join.JoinTrigger>')
+
+    def test__generate_jointrigger_param__input_empty_job_type_list__return_empty_string(self):
+        self.assertEqual(self.jj.generate_jointrigger_param([], 'true'), '')
+
+    def test__generate_jointrigger_param__input_empty_job_type_list__return_empty_string2(self):
+        self.assertEqual(self.jj.generate_jointrigger_param([], ''), '')
+
+    def test__generate_jointrigger_param__input_empty_unstable_behavior_string__raise_exception(self):
+        self.assertRaises(Exception, self.jj.generate_jointrigger_param, self.job_type_test_list, '')
+
+    def test__generate_jointrigger_param__input_invalid_unstable_behavior_string__raise_exception(self):
+        self.assertRaises(Exception, self.jj.generate_jointrigger_param, self.job_type_test_list, 'invalid')
+
+    # Testing generate_postbuildtrigger_param
+    def test__generate_postbuiltrigger_param__input_job_type_list_and_threshold_name_string__return_postbuildtrigger_config_string(self):
+        treshold_name_test = 'SUCCESS'
+        result = self.jj.generate_postbuildtrigger_param(self.job_type_test_list, treshold_name_test)
+        self.assertEqual(result, '<hudson.tasks.BuildTrigger> <childProjects>test-user__pipe_starter, test-user__prio_build, test-user__normal_build</childProjects> <threshold> <name>SUCCESS</name> </threshold> </hudson.tasks.BuildTrigger>')
+
+    def test__generate_postbuildtrigger_param__input_empty_job_type_list__return_empty_string(self):
+        self.assertEqual(self.jj.generate_postbuildtrigger_param([], ''), '')
+
+    def test__generate_postbuildtrigger_param__input_empty_job_type_list__return_empty_string2(self):
+        self.assertEqual(self.jj.generate_postbuildtrigger_param([], 'SUCCESS'), '')
+
+    def test__generate_postbuildtrigger_param__input_empty_threshold_name_string__raise_exception(self):
+        self.assertRaises(Exception, self.jj.generate_postbuildtrigger_param, self.job_type_test_list, '')
+
+    def test__generate_postbuildtrigger_param__input_invalid_threshold_name_string__raise_exception(self):
+        self.assertRaises(Exception, self.jj.generate_postbuildtrigger_param, self.job_type_test_list, 'INVALID')
+
+    # Testing generate_pipelinetrigger_param
+    def test__generate_pipelinetrigger_param__input_job_type_list__return_pipelinetrigger_config_string(self):
+        result = self.jj.generate_pipelinetrigger_param(self.job_type_test_list)
+        self.assertEqual(result, '<au.com.centrumsystems.hudson.plugin.buildpipeline.trigger.BuildPipelineTrigger> <downstreamProjectNames>test-user__pipe_starter, test-user__prio_build, test-user__normal_build</downstreamProjectNames> </au.com.centrumsystems.hudson.plugin.buildpipeline.trigger.BuildPipelineTrigger>')
+
+    def test__generate_pipelinetrigger_param__input_job_type_list__return_pipelinetrigger_config_string2(self):
+        result = self.jj.generate_pipelinetrigger_param(['pipe'])
+        self.assertEqual(result, '<au.com.centrumsystems.hudson.plugin.buildpipeline.trigger.BuildPipelineTrigger> <downstreamProjectNames>test-user__pipe_starter</downstreamProjectNames> </au.com.centrumsystems.hudson.plugin.buildpipeline.trigger.BuildPipelineTrigger>')
+
+    def test__generate_pipelinetrigger_param__input_empty_job_type_list__return_empty_string(self):
+        self.assertEqual(self.jj.generate_pipelinetrigger_param([]), '')
+
+    # Testing generate_groovypostbuild_param
+    def test__generate_groovypostbuild_param__input_script_type_string_project_list_behavior_string__return_groovypostbuild_config_string(self):
+        result = self.jj.generate_groovypostbuild_param('enable', self.job_type_test_list, 2)
+        self.assertEqual(result, "<org.jvnet.hudson.plugins.groovypostbuild.GroovyPostbuildRecorder> <groovyScript>if(manager.build.result.isBetterOrEqualTo(hudson.model.Result.FAILURE)) { manager.listener.logger.println('Because this build did not fail:' for (project in ['test-user__pipe_starter', 'test-user__prio_build', 'test-user__normal_build']) { manager.listener.logger.println(' - ' + project) manager.hudson.getItem(project).enable() } manager.listener.logger.println('will be enabled.'}</groovyScript> <behavior>2</behavior> </org.jvnet.hudson.plugins.groovypostbuild.GroovyPostbuildRecorder>")
+
+    def test__generate_groovypostbuild_param__input_script_type_string_project_list_behavior_string__return_groovypostbuild_config_string2(self):
+        result = self.jj.generate_groovypostbuild_param('disable', self.job_type_test_list, 2)
+        self.assertEqual(result, "<org.jvnet.hudson.plugins.groovypostbuild.GroovyPostbuildRecorder> <groovyScript>for (project in ['test-user__pipe_starter', 'test-user__prio_build', 'test-user__normal_build']) { manager.listener.logger.println('Disable ' + project + ' job') manager.hudson.getItem(project).disable() }</groovyScript> <behavior>2</behavior> </org.jvnet.hudson.plugins.groovypostbuild.GroovyPostbuildRecorder>")
+
+    def test__generate_groovypostbuild_param__input_empty_script_type_string__raise_exception(self):
+        self.assertRaises(Exception, self.jj.generate_groovypostbuild_param, '', self.job_type_test_list, 2)
+
+    def test__generate_groovypostbuild_param__input_invalid_script_type_string__raise_exception(self):
+        self.assertRaises(KeyError, self.jj.generate_groovypostbuild_param, 'invalid', self.job_type_test_list, 2)
+
+    def test__generate_groovypostbuild_param__input_empty_project_list__raise_exception(self):
+        self.assertRaises(Exception, self.jj.generate_groovypostbuild_param, 'enable', [], 2)
+
+    def test__generate_groovypostbuild_param__input_invalid_project_list_entry__raise_exception(self):
+        self.assertRaises(Exception, self.jj.generate_groovypostbuild_param, 'enable', ['invalid'], 2)
+
+    def test__generate_groovypostbuild_param__input_invalid_behavior__raise_exception(self):
+        self.assertRaises(Exception, self.jj.generate_groovypostbuild_param, 'enable', self.job_type_test_list, 3)
+
+    def test__generate_groovypostbuild_param__input_invalid_behavior__raise_exception2(self):
+        self.assertRaises(Exception, self.jj.generate_groovypostbuild_param, 'enable', self.job_type_test_list, -1)
+
+    def test__generate_groovypostbuild_param__input_invalid_behavior__raise_exception3(self):
+        self.assertRaises(Exception, self.jj.generate_groovypostbuild_param, 'enable', self.job_type_test_list, '1')
