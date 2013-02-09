afb81dbc2fd43ecfc9c2ccf8bb8fca223f96a848
fmw-jk <jannik.kett@googlemail.com>
2013-02-09 16:06:54 +0100

    moved function to cob_common

--- src/jenkins_setup/cob_build.py
+++ src/jenkins_setup/cob_build.py
@@ -3,40 +3,13 @@
 from jenkins_setup import cob_common
 
 
-def apt_get_install_also_nonrosdep(pkgs, ros_distro, rosdep=None, sudo=False):
+class CobBuilder(object):
     """
-    Extends cob_common.apt_get_install by trying to guess Debian package names
-    of packages not included in rosdep
-
-    @param pkgs: names of ros repositories
-    @type  pkgs: list
-    @param ros_distro: name of ros release, e.g. fuerte
-    @type  ros_distro: str
-    @param rosdep: rosdep resolver object
-    @type  rosdep: rosdep.RosDepResolver
-    @param sudo: execute command as super-user
-    @type  sudo: bool
+    Builder class
     """
 
-    rosdep_pkgs = []
-    aptget_pkgs = []
+    def __init__(self):
+        """
+        """
 
-    for pkg in pkgs:
-        if rosdep:
-            if rosdep.has_ros(pkg):
-                rosdep_pkgs.append(pkg)
-                continue
-        # TODO use python apt module to check if Debian package exists
-        aptget_pkgs.append('-'.join(['ros', ros_distro, pkg.replace('_', '-')]))
-
-    if rosdep_pkgs != []:
-        try:
-            cob_common.apt_get_install(rosdep_pkgs, rosdep, sudo)
-        except:
-            cob_common.BuildException("Failed to apt-get install rosdep packages.")
-
-    if aptget_pkgs != []:
-        try:
-            cob_common.apt_get_install(aptget_pkgs, sudo=sudo)
-        except:
-            cob_common.BuildException("Failed to apt-get install ros repositories")
+        pass
--- src/jenkins_setup/cob_common.py
+++ src/jenkins_setup/cob_common.py
@@ -39,6 +39,45 @@ def apt_get_install(pkgs, rosdep=None, sudo=False):
         print "Not installing anything from apt right now."
 
 
+def apt_get_install_also_nonrosdep(pkgs, ros_distro, rosdep=None, sudo=False):
+    """
+    Extends cob_common.apt_get_install by trying to guess Debian package names
+    of packages not included in rosdep
+
+    @param pkgs: names of ros repositories
+    @type  pkgs: list
+    @param ros_distro: name of ros release, e.g. fuerte
+    @type  ros_distro: str
+    @param rosdep: rosdep resolver object
+    @type  rosdep: rosdep.RosDepResolver
+    @param sudo: execute command as super-user
+    @type  sudo: bool
+    """
+
+    rosdep_pkgs = []
+    aptget_pkgs = []
+
+    for pkg in pkgs:
+        if rosdep:
+            if rosdep.has_ros(pkg):
+                rosdep_pkgs.append(pkg)
+                continue
+        # TODO use python apt module to check if Debian package exists
+        aptget_pkgs.append('-'.join(['ros', ros_distro, pkg.replace('_', '-')]))
+
+    if rosdep_pkgs != []:
+        try:
+            apt_get_install(rosdep_pkgs, rosdep, sudo)
+        except:
+            BuildException("Failed to apt-get install rosdep packages.")
+
+    if aptget_pkgs != []:
+        try:
+            apt_get_install(aptget_pkgs, sudo=sudo)
+        except:
+            BuildException("Failed to apt-get install ros repositories")
+
+
 def copy_test_results(workspace, buildspace, errors=None, prefix='dummy'):
     print "Preparing xml test results"
     try:
