c01b3305bf56ec886684e7b26d834dd37c83e44a
ipa320 <florian.weisshardt@ipa.fhg.de>
2013-06-17 11:51:05 +0200

    disabled exit

--- scripts/graphicTest/setupGraphicEnv.bash
+++ scripts/graphicTest/setupGraphicEnv.bash
@@ -3,7 +3,7 @@ export WORKSPACE=$1
 . $WORKSPACE/env_vars.sh
 export DIR=$WORKSPACE/jenkins_setup/scripts/graphicTest
 env
-exit 1
+#exit 1
 
 $WORKSPACE/setupGraphicDriver.bash
 $DIR/installPackages.bash
