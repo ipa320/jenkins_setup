53cb5288fe0b642d1d99f061c5d2deaab7df419d
root <root@cob-sim2.(none)>
2013-06-20 12:45:38 +0000

    Reinstall libgl1-mesa-glx
    On Lucid 64-bit we otherwise encounter the following error:
    'nvidia return status for module build on kernel: 2.6.32-48-generic'

--- scripts/graphicTest/chroot/installNvidia.bash
+++ scripts/graphicTest/chroot/installNvidia.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install -y --force-yes nvidia-current
+apt-get install -y --force-yes --reinstall libgl1-mesa-glx nvidia-current
 if [ $? != 0 ]; then
     echo ''
     echo '---------------------------------------------------------'
