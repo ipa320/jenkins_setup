98687e7ab596760bb8697b3e17209b73653c7eda
Simon Ebner <Simon.Ebner@gmail.com>
2013-06-20 13:35:54 +0200

    Print warnings/errors if installation of packages fails

--- scripts/graphicTest/chroot/installSimulator.bash
+++ scripts/graphicTest/chroot/installSimulator.bash
@@ -1,7 +1,31 @@
 #!/bin/bash
 apt-get install -y ros-electric-care-o-bot ros-electric-desktop-full ros-electric-simulator-gazebo ros-electric-pr2
+if [ $? != 0 ]; then
+    echo ''
+    echo '----------------------------------------------'
+    echo 'Error occured during installation of simulator'
+    echo '----------------------------------------------'
+    echo ''
+    exit 1
+fi
+
 apt-get install -y mysql-common libmysqlclient16 libmysqlclient-dev libmysqlclient16-dev
+if [ $? != 0 ]; then
+    echo ''
+    echo '----------------------------------------------'
+    echo 'Warning: Could not install important packages'
+    echo '----------------------------------------------'
+    echo ''
+fi
+
 apt-get install -y libwxgtk2.8-0 libwxgtk2.8-dev python-wxgtk2.8 python-wxversion wx2.8-headers
+if [ $? != 0 ]; then
+    echo ''
+    echo '----------------------------------------------'
+    echo 'Warning: Could not install important packages'
+    echo '----------------------------------------------'
+    echo ''
+fi
 
 #dpkg -l > $WORKSPACE/dpkg_output.log
 
