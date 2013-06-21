c46012ec235daec9d868e05ebcce795a2e3112ac
Simon Ebner <Simon.Ebner@gmail.com>
2013-06-21 11:55:26 +0200

    Compatible to nvidia-experimental-304
    More thoughtful install script in chroot. Not only copy content
    of data.tar.gz to / but also modify postinst in control.tar.gz
    and run afterwads

--- scripts/graphicTest/host/downloadNvidiaDriver.bash
+++ scripts/graphicTest/host/downloadNvidiaDriver.bash
@@ -4,29 +4,43 @@ if [ -d /tmp/nvidia ]; then
     exit 0
 fi
 
-VERSION=`dpkg -l | grep nvidia-current | sed -rn "s/.*nvidia-current\s*([^ ]+).*/\1/p"`
-if [ -z $VERSION ]; then
-    echo 'No nvidia-current driver installed'
-    exit 1
-fi
+for driver in "nvidia-current" "nvidia-experimental-304"; do
+    echo ""
+    echo "Searching for '$driver'"
+    echo ""
 
+    VERSION=`dpkg -l | grep "$driver" | sed -rn "s/.*$driver\s*([^ ]+).*/\1/p"`
+    if [ -z $VERSION ]; then
+        echo "'$driver' not found"
+        continue
+    fi
 
-endpoint=`apt-cache --no-all-versions policy nvidia-current | grep -A1 $VERSION | tail -n 1 | awk '{ print $2 }'`
-if [ -z $endpoint ]; then
-    echo 'No endpoint found'
-    exit 2
-fi
+    endpoint=`apt-cache --no-all-versions policy $driver | grep -A1 $VERSION | tail -n 1 | awk '{ print $2 }'`
+    if [ -z $endpoint ]; then
+        echo 'No endpoint found'
+        exit 2
+    fi
 
-filename=`apt-cache --no-all-versions show nvidia-current=$VERSION | grep Filename | awk '{ print $2 }'`
-if [ -z $filename ]; then 
-    echo 'No filename found'
-    exit 3
-fi
+    filename=`apt-cache --no-all-versions show $driver=$VERSION | grep Filename | awk '{ print $2 }'`
+    if [ -z $filename ]; then 
+        echo 'No filename found'
+        exit 3
+    fi
 
-uri="$endpoint$filename"
-uriI386=`echo $uri|sed -r 's/(i386|amd64)/i386/g'`
-uriAMD64=`echo $uri|sed -r 's/(i386|amd64)/amd64/g'`
+    uri="$endpoint$filename"
+    uriI386=`echo $uri|sed -r 's/(i386|amd64)/i386/g'`
+    uriAMD64=`echo $uri|sed -r 's/(i386|amd64)/amd64/g'`
 
-rm -rf /tmp/nvidia
-wget $uriI386 -P /tmp/nvidia
-wget $uriAMD64 -P /tmp/nvidia
+    rm -rf /tmp/nvidia
+    wget $uriI386 -P /tmp/nvidia
+    wget $uriAMD64 -P /tmp/nvidia
+
+    exit 0
+done
+
+echo ""
+echo "-------------------------------------------------"
+echo "Error: Could not find any nvidia-driver installed"
+echo "-------------------------------------------------"
+echo ""
+exit 4
