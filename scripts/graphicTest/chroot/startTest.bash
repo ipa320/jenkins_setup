40292ff017687ce401be686cada021faf709e633
SimonEbner <Simon.Ebner@gmail.com>
2013-06-25 15:30:05 +0200

    Use rosmake --test-only

--- scripts/graphicTest/chroot/startTest.bash
+++ scripts/graphicTest/chroot/startTest.bash
@@ -2,4 +2,4 @@
 source /opt/ros/electric/setup.bash
 export ROS_PACKAGE_PATH=~/ros:$ROS_PACKAGE_PATH
 VGL=/opt/VirtualGL/bin/vglrun
-$VGL rostest navigation_test navigation.test robot:=cob3-3 robot_env:=ipa-kitchen route:=1
+$VGL rosmake --test-only
