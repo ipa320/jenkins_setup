372773936b71e97c63b788b9d6391165c868ed43
ipa320 <florian.weisshardt@ipa.fhg.de>
2013-06-27 17:33:45 +0200

    Copy ssh keys from  to ~ and /root

--- scripts/graphicTest/chroot/setupSSHKeys.bash
+++ scripts/graphicTest/chroot/setupSSHKeys.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
-if [ ! -d ~/.ssh ] && [ ! -d /root/.ssh ]; then
-    echo 'No .ssh keys in ~/.ssh or /root/.ssh found'
+if [ ! -d $WORKSPACE/.ssh ]; then
+    echo 'No .ssh keys in $WORKSPACE/.ssh found'
     exit 1
 fi
-if [ -d ~/.ssh ] && [ ! -d /root/.ssh ]; then
-    cp -r ~/.ssh /root/
-fi
-if [ -d /root/.ssh ] && [ ! -d ~/.ssh ]; then
-    cp -r /root/.ssh ~/
-fi
+cp -r $WORKSPACE/.ssh ~/
+cp -r $WORKSPACE/.ssh /root/
+chmod 700 ~/.ssh
+chmod 700 /root/.ssh
+
+user=`whoami`
+chown -R $user:$user ~/.ssh
+chown -R $user:$user /root/.ssh
