06b14fa27c66422e60f459a405c6918821ef7029
fmw-jk <jannik.kett@googlemail.com>
2013-08-27 12:50:26 +0200

    added list of modules

--- src/jenkins_setup/__init__.py
+++ src/jenkins_setup/__init__.py
@@ -1,3 +1,10 @@
 """
-Module to automatically generate Jenkins cob-pipeline jobs in combination with the cob-pipeline-plugin. For more information read the README.md.
+Package to automatically generate Jenkins cob-pipeline jobs in combination with
+the cob-pipeline-plugin. For more information read the README.md.
+Included modules:
+    cob_develdistro.py
+    cob_pipe.py
+    common.py
+    jenkins_job_creator.py
+    rosdep.py
 """
