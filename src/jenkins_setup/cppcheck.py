60f06135d4afd27f75e72d39d6c30518d0c60073
ipa-fmw <fmw@ipa.fhg.de>
2013-10-21 11:01:01 +0200

    add try except to cppcheck

--- src/jenkins_setup/cppcheck.py
+++ src/jenkins_setup/cppcheck.py
@@ -4,14 +4,17 @@ import subprocess
 from jenkins_setup import common
 
 def run(source_path, result_path):
-    print "===== Performing CPP check ====="
+    print "===== Performing cppcheck ====="
 
-    out, err = common.call("cppcheck --enable=all -j8 -q --xml " + str(source_path), verbose=False)
-    common.call("touch " + result_path + "/cppcheck.xml")
-    f = open(result_path + "/cppcheck.xml", 'w')
-    f.write(err)
+    try: 
+        out, err = common.call("cppcheck --enable=all -j8 -q --xml " + str(source_path), verbose=False)
+        common.call("touch " + result_path + "/cppcheck.xml")
+        f = open(result_path + "/cppcheck.xml", 'w')
+        f.write(err)
+    except:
+        print "Error in cppcheck. Skipping test, no results generated."
 
-    print "===== Completed CPP check ====="
+    print "===== Completed cppcheck ====="
 
 if __name__ == "__main__":
     run("/tmp/src","/tmp/result")
