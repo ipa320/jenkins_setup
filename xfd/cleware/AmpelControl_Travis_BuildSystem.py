e78818e58c6ecd2e4f87c03dd1b224db38dab43b
Florian Weisshardt <florian.weisshardt@ipa.fraunhofer.de>
2016-10-06 16:03:38 +0200

    ampel using travispy python client

--- xfd/cleware/AmpelControl_Travis_BuildSystem.py
+++ xfd/cleware/AmpelControl_Travis_BuildSystem.py
@@ -2,12 +2,11 @@
 
 import os
 import sys
-import urllib2
 import time
 import subprocess
 from optparse import OptionParser
 from thread import start_new_thread
-import json
+import travispy
 
 
 class ampel_state:
@@ -25,13 +24,6 @@ class ampel_state:
     def get_state(self):
         return self.state
 
-
-# color: (0=red, 1=yellow, 2=blue/green)
-# mode: (0=off, 1=on, 2=flash)
-# red: if build state Failure/Error for at least one repo
-# yellow flash: if at least one repo is Active
-# green: if all repo build state Success
-# all flash: if any of the build url is invalid/no internet connection
 def extract_ampel_state(state):
     message = "Ampel is "
     state_out = [0,0,0]
@@ -57,10 +49,10 @@ def extract_ampel_state(state):
         state_out[1] = 0
 
     message += " and "
-    if "blue_anime" in state or "grey_anime" in state:
+    if "green_anime" in state:
         message += "flashing green"
         state_out[2] = 2
-    elif "blue" in state:
+    elif "green" in state:
         message += "green"
         state_out[2] = 1
     else:
@@ -97,7 +89,6 @@ def extract_build_query(file_path):
     tag_store = []
     with open(file_path) as fp:
         for line in fp:
-            #print line
             if "git:" in line:
                 user = None
                 repo = None
@@ -122,103 +113,55 @@ def extract_build_query(file_path):
     return tag_store
     
 
-def extract_travis_build_states(path, auth_token):
-    '''
-    :construct URI to get minimalistic information 
-    regarding the build-status of each repos
-    :We could not identify if the repo is a private or public one,
-    modify URI for public/private repo and get build status data
-    '''
-    url_string = None
-    url = "https://api.travis-ci."
-    private_domain = "com"
-    public_domain = "org"
-    
-    build_query_data = extract_build_query(path)
+def extract_travis_build_states(path, t_public, t_private):
     state_store = []
-    
-    checker_build_failure = False
-    checker_repo_active = False
-    checker_invalid_state = False
-    
-    print "\nChecking build states for repositories....\n"
-    
-    ### iterate over all the repos and get their travis build states
+    build_query_data = extract_build_query(options.path)
+
+    build_states = []
     for data in build_query_data:
-        owner = data["user"]
-        repo_name = data["repo"]
-        branch = data["branch"]
-        ### on valid owner, repo_name and branch, fire the urls and get travis buld states
-        if owner != "" and repo_name != "" and branch != "":
-            print "[Owner:",owner,"]  [Repository:",repo_name,"]  [Branch:",branch,"]"
-            ### first try to get json build data assuming the repo is public
+        invalid_state = False
+        # try public server
+        try:
+            branch = t_public.branch(data['branch'], data['user'] + '/' + data['repo'])
+            build_states.append(branch.state)
+        except travispy.errors.TravisError, e:
+            # try private server
             try:
-                url_string = url+public_domain+"/repos/"+owner+'/'+repo_name+".json?branch="+branch 
-                response_data = urllib2.urlopen(url_string)
-            except urllib2.HTTPError, e:
-                response_data = ""
-                checker_invalid_state = True
-                print e,", trying private url"
-            except urllib2.URLError, e:
-                response_data = ""
-                checker_invalid_state = True
-                print e,", trying private url"
-            ### second try to get json build data assuming the repo is private
-            if response_data == "":
-                try:
-                    url_string = url+private_domain+"/repos/"+owner+'/'+repo_name+".json?token="+auth_token+"&branch="+branch
-                    response_data = urllib2.urlopen(url_string)
-                except urllib2.HTTPError, e:
-                    response_data = ""
-                    checker_invalid_state = True
-                    print e, "-------------------------------------------"
-                except urllib2.URLError, e:
-                    response_data = ""
-                    checker_invalid_state = True
-                    print e, "-----------------------------------------------"
-            ### if success in getting response (from public/private repo), we get valid build states, now check each state result
-            if response_data != "":
-                checker_invalid_state = False
-                build_data = json.loads(response_data.read())
-                for entry in build_data.items():
-                    if entry[0]:
-                        if entry[0] == "last_build_id":
-                            if entry[1] == None:
-                                checker_invalid_state =  True
-                                break
-                        #if entry[0] == "last_build_status":
-                        if entry[0] == "last_build_result":
-                            if entry[1] == 1: ### build falied/error
-                                checker_build_failure = True
-                                print "---> Build failed for repository:", repo_name
-                        if entry[0] == "last_build_duration":
-                            if entry[1] == None: ### repo is active
-                                checker_repo_active = True
-                                print "---> Active repository:", repo_name
-                    else:
-                        print "No build state found for:"
-                        print "[Owner*:",owner,"]  [Repository*:",repo_name,"]  [Branch*:",branch,"]"
-                        ### if an invalid state is encountered (possibly wrong url), quickly alert
-            if checker_invalid_state:
-                break
-        else:
-            checker_invalid_state = True
-            print "Found invalid owner/repository/branch name, please check the supplied links:"
-            print "[Owner*:",owner,"]  [Repository*:",repo_name,"]  [Branch*:",branch,"]" 
-            
-    ### varify the build states and set flags
-    if checker_invalid_state:
-        print "\nThere is an invalid build url for repository:",repo_name," Please check manually !!\n"
-        state_store.append("blue_anime")
-        state_store.append("red_anime")
-        state_store.append("yellow_anime")
-    else:
-        if checker_build_failure:
-            state_store.append("red")
-        else:
-            state_store.append("blue")
-        if checker_repo_active:
-            state_store.append("yellow_anime")
+                #print "no public repo, trying private url. error:", e
+                branch = t_private.branch(data['branch'], data['user'] + '/' + data['repo'])
+                build_states.append(branch.state)
+            except Exception, e:
+                invalid_state = True
+        except Exception, e:
+            invalid_state = True
+
+        # check for invalid state
+        if invalid_state:
+            print "error for " + data['user'] + '/' + data['repo']  + " in branch " + data['branch'] + ": ", e
+            state_store.append('red_anime')
+            state_store.append('yellow_anime')
+            state_store.append('green_anime')
+            return state_store
+
+    print "build_states", build_states
+
+    ### set red ###
+    # check for failed builds
+    if 'failed' in build_states:
+        state_store.append("red")
+    
+    ### set yellow ###
+    # check for ready to merge PRs
+    # TODO (flash yellow)
+        
+    ### set green ###
+    # check for ongoing builds
+    if 'created' in build_states or 'started' in build_states or 'queued' in build_states:
+        state_store.append("green_anime")
+    # all builds ok
+    if 'red' not in state_store and 'red_amine' not in state_store:
+        state_store.append("green")
+
     return state_store
 
 
@@ -235,7 +178,7 @@ if __name__ == "__main__":
         help="The ROSInstall file path for parsing repositories URLs to monitor build states.")
     parser.add_option("-t", "--token",
         dest="token",
-        default=None,
+        default="9e525c41759d9c95919308d92a6747088c580661",
         help="Please give github token, this is different than one found on your github profile")
     parser.add_option("-d", "--device",
         dest="device",
@@ -249,18 +192,20 @@ if __name__ == "__main__":
     # initial reset the ample states
     ampel_state = [0,0,0]
     ampel.set_state(ampel_state)
-    if options.token == None:
-        options.token = "8SejjXqqq1WHebgRttQF"  ### assigning a default token
+
+
+    t_public = travispy.TravisPy.github_auth(options.token, travispy.travispy.PUBLIC)
+    t_private = travispy.TravisPy.github_auth(options.token, travispy.travispy.PRIVATE)
 
     while True:
-        try:
-            travis_state = extract_travis_build_states(options.path, options.token)
-            ampel_state, message = extract_ampel_state(travis_state)
-            print "\n[",message,"]" , "Ample States:",ampel_state
-            ampel.set_state(ampel_state)
-        except Exception, e:
-            print e
-            ampel.set_state([2, 2, 2])
-            pass
+#        try:
+        travis_state = extract_travis_build_states(options.path, t_public, t_private)
+        ampel_state, message = extract_ampel_state(travis_state)
+        print "\n[",message,"]" , "Ample States:",ampel_state
+        ampel.set_state(ampel_state)
+#        except Exception, e:
+#            print e
+#            ampel.set_state([2, 2, 2])
+#            pass
         time.sleep(10)
 
